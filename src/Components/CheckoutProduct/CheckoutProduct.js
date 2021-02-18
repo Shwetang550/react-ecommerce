5208bf1c20286397c5f5fccf585dcc269b7a69d4
Shwetang <shwetangsingh5@gmail.com>
2021-02-18 13:24:53 +0530

    checkoutProduct aligned

--- src/Components/CheckoutProduct/CheckoutProduct.js
+++ src/Components/CheckoutProduct/CheckoutProduct.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './CheckoutProduct.css';
+import { useStateValue } from '../../StateProvider';
 
 const CheckoutProduct = ({ id, image, title, price, rating }) => {
+    const [{ cart }, dispatch] = useStateValue();
+
+    const handleDelete = () => { 
+        dispatch({
+            type: 'REMOVE_FROM_CART',
+            id: id,
+        })
+    };
+
     return (  
         <div className="checkoutProduct">
             <img
@@ -24,7 +34,12 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
                     }
                 </div>
 
-                <button>Remove from Cart</button>
+                <button
+                    onClick={handleDelete}
+                    style={{ cursor: 'pointer' }}
+                >
+                    Remove from Cart
+                </button>
             </div>
         </div>
     );
